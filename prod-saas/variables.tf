0cda3b0db941abb58707a53ddce6688baa97046c
Avi Cavale <avi@shippable.com>
2016-10-26 12:24:23 -0700

    adding prod deployment

---
+++ prod-saas/variables.tf
@@ -0,0 +1,106 @@
+# main creds for AWS connection
+variable "aws_access_key_id" {
+  description = "AWS access key"
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secert access key"
+}
+
+variable "region" {
+  description = "AWS region"
+  default = "us-east-1"
+}
+
+variable "avl-zone" {
+  description = "availability zone used for the beta"
+  default = "us-east-1b"
+}
+
+# this is a keyName for key pairs
+variable "aws_key_name" {
+  description = "Key Pair Name used to login to the box"
+  default = "prod-us-east-1"
+}
+
+# this is a PEM key for key pairs
+variable "aws_key_filename" {
+  description = "Key Pair FileName used to login to the box"
+  default = "prod-us-east-1.pem"
+}
+
+# all variables related to VPC
+variable "install_version" {
+  description = "version of the infra"
+  default = "prodSaas"
+}
+
+
+# START all variables related to VPC !!! HARD CODEDED !!!
+# we need to hard code it as we dont want to create it from scratch due to mongo
+
+variable "vpc_id" {
+  description = "Prod VPC ID"
+  default = "vpc-2d9a894f"
+}
+
+variable "sn_public_ship_id" {
+  description = "Prod Public Subnet ID"
+  default = "subnet-42bfef04"
+}
+
+variable "sn_ship_install_id" {
+  description = "Prod Public Subnet ID"
+  default = "subnet-45bfef03"
+}
+
+variable "in_msg_id" {
+  description = "Prod NAT Instance ID"
+  default = "i-a380068e"
+}
+
+# END all variables related to VPC !!! HARD CODEDED !!!
+
+variable "cidr_block" {
+  description = "Uber IP addressing for the Network"
+  default = "20.0.0.0/16"
+}
+
+variable "cidr_public_ship" {
+  description = "Public 0.0 CIDR for externally accesible subnet"
+  default = "20.0.0.0/24"
+}
+
+variable "cidr_private_ship_install" {
+  description = "Private 0.2 block for shippable services"
+  default = "20.0.10.0/24"
+}
+
+variable "in_type_core" {
+  //make sure it is compatible with AMI, not all AMIs allow all instance types "
+  default = "t2.medium"
+  description = "AWS Instance type for Core server"
+}
+
+variable "in_type_nat" {
+  //make sure it is compatible with AMI, not all AMIs allow all instance types "
+  default = "t2.medium"
+  description = "AWS Instance type for NAT server"
+}
+
+variable "ami_us_east_1_ubuntu1404"{
+  default = "ami-2d39803a"
+  description = "AWS AMI for us-east-1 Ubuntu 14.04"
+}
+
+# this is a special ami preconfigured to do NAT
+variable "ami_us_east_1_nat"{
+  default = "ami-d2ee95c5"
+  description = "NAT AMI for us-east-1"
+}
+
+# this is a ACM certificate for domian *.shippable.com *.qhode.com
+variable "acm_cert_arn"{
+  default = "arn:aws:acm:us-east-1:983672658014:certificate/592e0545-d0a2-4da2-a1fe-c3d7c0e332c0"
+  description = "acm cert arn"
+}
---
+++ prod-saas/vpc.tf
@@ -0,0 +1,152 @@
+#========================== VPC  =============================
+
+# Internet gateway for the public subnet
+resource "aws_internet_gateway" "ig" {
+  vpc_id = "${var.vpc_id}"
+  tags {
+    Name = "ig_${var.install_version}"
+  }
+}
+
+#========================== 0.0 Subnet =============================
+
+# Routing table for public subnet
+resource "aws_route_table" "rt_public" {
+  vpc_id = "${var.vpc_id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.ig.id}"
+  }
+  tags {
+    Name = "rt-public_${var.install_version}"
+  }
+}
+
+//# Associate the routing table to public subnet
+//resource "aws_route_table_association" "rt_assn_public" {
+//  subnet_id = "${var.sn_public_ship_id}"
+//  route_table_id = "${aws_route_table.rt_public.id}"
+//}
+
+#========================== inst subnet ======================
+# Routing table for private subnet
+resource "aws_route_table" "rt_ship_install" {
+  vpc_id = "${var.vpc_id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    instance_id = "${aws_instance.nat.id}"
+  }
+  tags {
+    Name = "rt_ship_install_${var.install_version}"
+  }
+}
+
+//# Associate the routing table to private subnet
+//resource "aws_route_table_association" "rt_assn_ship_install" {
+//  subnet_id = "${var.sn_ship_install_id}"
+//  route_table_id = "${aws_route_table.rt_ship_install.id}"
+//}
+
+#========================== NAT =============================
+
+# NAT SG
+resource "aws_security_group" "sg_public_nat" {
+  name = "sg_public_nat_${var.install_version}"
+  description = "Allow traffic to pass from the private subnet to the internet"
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "${var.cidr_private_ship_install}"
+    ]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [
+      "${var.cidr_block}"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  vpc_id = "${var.vpc_id}"
+
+  tags {
+    Name = "sg_public_nat_${var.install_version}"
+  }
+}
+
+//this is a hack to get around double interpolation issues
+//We need this in provisioner file block down below
+resource "null_resource" "pemfile" {
+  triggers {
+    fileName = "${var.aws_key_filename}"
+  }
+}
+
+# NAT Server
+resource "aws_instance" "nat" {
+  ami = "${var.ami_us_east_1_nat}"
+  availability_zone = "${var.avl-zone}"
+  instance_type = "${var.in_type_nat}"
+  key_name = "${var.aws_key_name}"
+
+  subnet_id = "${var.sn_public_ship_id}"
+  vpc_security_group_ids = [
+    "${aws_security_group.sg_public_nat.id}"]
+
+  provisioner "file" {
+    source = "${var.aws_key_filename}"
+    destination = "~/.ssh/${var.aws_key_filename}"
+
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      private_key = "${file(null_resource.pemfile.triggers.fileName)}"
+      agent = true
+    }
+  }
+
+  associate_public_ip_address = true
+  source_dest_check = false
+
+  tags = {
+    Name = "nat_${var.install_version}"
+  }
+}
+
+# Associate EIP, without this private SN wont work
+resource "aws_eip" "nat" {
+  instance = "${aws_instance.nat.id}"
+  vpc = true
+}
+
+//# make this routing table the main one
+//resource "aws_main_route_table_association" "rt_main_ship_install" {
+//  vpc_id = "${var.vpc_id}"
+//  route_table_id = "${aws_route_table.rt_ship_install.id}"
+//}

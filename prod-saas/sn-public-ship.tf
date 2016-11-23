2df6bf9d22f5fe64c1aa443f69bdfb064e8e6fe4
Avi Cavale <avi@shippable.com>
2016-11-23 11:33:38 +0530

    testing the plan out for prod-saas

--- prod-saas/sn-public-ship.tf
+++ prod-saas/sn-public-ship.tf
@@ -211,10 +211,10 @@ resource "aws_elb" "lb_api" {
 
   health_check {
     healthy_threshold = 2
-    unhealthy_threshold = 5
+    unhealthy_threshold = 2
     timeout = 10
     target = "HTTP:50000/"
-    interval = 2
+    interval = 5
   }
 
   instances = [

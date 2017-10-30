9dca08d7ddcdf17dd025bccfcbf9879c82e6b53c
Devashish <devashish.86@gmail.com>
2017-10-30 10:02:18 +0530

    adding custom node to test grisham builds

--- rc-saas/sn-private-ship-install.tf
+++ rc-saas/sn-private-ship-install.tf
@@ -560,6 +560,31 @@ output "test_inst_1_centos_7" {
   value = "${aws_instance.test_inst_1_centos_7.private_ip}"
 }
 
+resource "aws_instance" "grisham_test_u1604" {
+  ami = "${var.ami_us_east_1_ubuntu1604}"
+  availability_zone = "${var.avl-zone}"
+  instance_type = "${var.in_type_core}"
+  key_name = "${var.aws_key_name}"
+  subnet_id = "${aws_subnet.sn_ship_install.id}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.sg_private_ship_install.id}"]
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 50
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "grisham_test_u1604_${var.install_version}"
+  }
+}
+
+output "grisham_test_u1604" {
+  value = "${aws_instance.grisham_test_u1604.private_ip}"
+}
+
 # ---------------
 # temporary ebs volume for migrating RC db
 # ---------------

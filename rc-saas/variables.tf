94a426d7299a0ca65bc0fe25e421541a7d9daaee
Devashish <devashish.86@gmail.com>
2018-01-04 11:43:25 +0530

    adding build hosts for production

--- rc-saas/variables.tf
+++ rc-saas/variables.tf
@@ -71,6 +71,12 @@ variable "in_type_core" {
   description = "AWS Instance type for consul server"
 }
 
+variable "in_type_bld" {
+  //make sure it is compatible with AMI, not all AMIs allow all instance types "
+  default = "c3.large"
+  description = "AWS Instance type for running builds"
+}
+
 variable "in_type_nat" {
   //make sure it is compatible with AMI, not all AMIs allow all instance types "
   default = "t2.small"

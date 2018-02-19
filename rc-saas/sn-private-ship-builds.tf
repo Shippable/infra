06968c035fb19d77f6a9e0606deee78986183526
mohit5it9 <mohit@shippable.com>
2018-02-19 12:16:00 +0530

    removes test instance for x86_64 u16

--- rc-saas/sn-private-ship-builds.tf
+++ rc-saas/sn-private-ship-builds.tf
@@ -151,32 +151,6 @@ output "shrivara_x86_64_c7" {
   value = "${aws_instance.shrivara_x86_64_c7.private_ip}"
 }
 
-##u16 test instance mohit
-resource "aws_instance" "mohit_x86_64_u1604" {
-  ami = "${var.ami_us_east_1_ubuntu1604}"
-  availability_zone = "${var.avl-zone}"
-  instance_type = "${var.in_type_core}"
-  key_name = "${var.aws_key_name}"
-  subnet_id = "${aws_subnet.sn_public.id}"
-
-  vpc_security_group_ids = [
-    "${aws_security_group.sg_private_ship_builds.id}"]
-
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 50
-    delete_on_termination = true
-  }
-
-  tags = {
-    Name = "mohit_x86_64_u1604_${var.install_version}"
-  }
-}
-
-output "mohit_x86_64_u1604" {
-  value = "${aws_instance.mohit_x86_64_u1604.private_ip}"
-}
-
 ## centOs test instance Rituraj
 resource "aws_instance" "rituraj_x86_64_c7" {
   ami = "${var.ami_us_east_1_centos7}"

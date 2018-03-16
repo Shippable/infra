2cd35cf474ea70f0f2f7fa8e732659557325bb51
Riturajcse <riturajcse1994@gmail.com>
2018-03-16 14:19:51 +0530

    adds machine to test my changes

--- rc-saas/sn-private-ship-builds.tf
+++ rc-saas/sn-private-ship-builds.tf
@@ -287,3 +287,28 @@ resource "aws_instance" "test_instance_centos_7_ric03uec" {
 output "test_instance_centos_7_ric03uec" {
   value = "${aws_instance.test_instance_centos_7_ric03uec.private_ip}"
 }
+
+resource "aws_instance" "test_instance_rituraj_u1604" {
+  ami = "${var.ami_us_east_1_ubuntu1604}"
+  availability_zone = "${var.avl-zone}"
+  instance_type = "${var.in_type_core}"
+  key_name = "${var.aws_key_name}"
+  subnet_id = "${aws_subnet.sn_public.id}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.sg_private_ship_builds.id}"]
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 50
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "test_instance_rituraj_u1604_${var.install_version}"
+  }
+}
+
+output "test_instance_rituraj_u1604" {
+  value = "${aws_instance.test_instance_rituraj_u1604.private_ip}"
+}

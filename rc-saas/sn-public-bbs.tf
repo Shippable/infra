e61428d208543f08f4f9f1dcb0e320886cfea5bc
Devashish <devashish.86@gmail.com>
2018-07-16 12:52:12 +0530

    splitting up instance and lb security group

--- rc-saas/sn-public-bbs.tf
+++ rc-saas/sn-public-bbs.tf
@@ -1,7 +1,15 @@
 resource "aws_security_group" "sg_public_bbs" {
   name = "sg_public_bbs_${var.install_version}"
-  description = "BBS traffic security group"
+  description = "BBS instance security group"
   vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
 
   ingress {
     from_port = 7990
@@ -20,9 +28,44 @@ resource "aws_security_group" "sg_public_bbs" {
   }
 
   ingress {
+    from_port = "22"
+    to_port = "22"
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  ## Allow all outbound traffic
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "sg_public_bbs_${var.install_version}"
+  }
+}
+
+resource "aws_security_group" "sg_public_lb_bbs" {
+  name = "sg_public_lb_bbs_${var.install_version}"
+  description = "BBS ELB security group"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
     from_port = -1
     to_port = -1
     protocol = "icmp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = [
       "0.0.0.0/0"]
   }
@@ -53,7 +96,7 @@ resource "aws_security_group" "sg_public_bbs" {
   }
 
   tags {
-    Name = "sg_public_bbs_${var.install_version}"
+    Name = "sg_public_lb_bbs_${var.install_version}"
   }
 }
 

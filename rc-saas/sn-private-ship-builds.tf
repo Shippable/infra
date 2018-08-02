80af7b073ce5d28ba066fee1cb28d1d3969973a1
Devashish <devashish.86@gmail.com>
2018-08-02 10:27:52 +0530

    fixing subnet for centos test hosts

--- rc-saas/sn-private-ship-builds.tf
+++ rc-saas/sn-private-ship-builds.tf
@@ -323,7 +323,7 @@ resource "aws_instance" "test_instance_ric03uec_centos7_01" {
   availability_zone = "${var.avl-zone}"
   instance_type = "${var.in_type_core}"
   key_name = "${var.aws_key_name}"
-  subnet_id = "${aws_subnet.sn_ship_install.id}"
+  subnet_id = "${aws_subnet.sn_public.id}"
 
   vpc_security_group_ids = [
    "${aws_security_group.sg_private_ship_builds.id}"]

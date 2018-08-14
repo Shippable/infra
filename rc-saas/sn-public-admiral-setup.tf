23870a389d34b3a919f5ff9d47ea60a537d4013c
Devashish <devashish.86@gmail.com>
2018-08-14 12:14:39 +0530

    testing u14 admiral setup

--- rc-saas/sn-public-admiral-setup.tf
+++ rc-saas/sn-public-admiral-setup.tf
@@ -181,3 +181,30 @@ resource "aws_instance" "admiral_ric03uec_u1604" {
 output "admiral_ric03uec_u1604" {
   value = "${formatlist("instance %v has private ip %v", aws_instance.admiral_ric03uec_u1604.*.id, aws_instance.admiral_ric03uec_u1604.*.private_ip)}"
 }
+
+# ric03uec: x86/64 Ubuntu 14.04 Instances
+resource "aws_instance" "admiral_ric03uec_u1404" {
+  ami = "${var.ami_us_east_1_ubuntu1404}"
+  availability_zone = "${var.avl-zone}"
+  instance_type = "${var.in_type_core}"
+  key_name = "${var.aws_key_name}"
+  subnet_id = "${aws_subnet.sn_public.id}"
+
+  count = 1
+  vpc_security_group_ids = [
+    "${aws_security_group.sg_private_ship_builds.id}"]
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 100
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "admiral_ric03uec_u1404_${count.index}_${var.install_version}"
+  }
+}
+
+output "admiral_ric03uec_u1404" {
+  value = "${formatlist("instance %v has private ip %v", aws_instance.admiral_ric03uec_u1404.*.id, aws_instance.admiral_ric03uec_u1404.*.private_ip)}"
+}

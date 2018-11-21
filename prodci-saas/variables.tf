9b74b78ff2c8095a6e89e8706aa1ac4421805859
jatindogra <dogra.jatin5@gmail.com>
2018-11-21 13:20:47 +0530

    adds pipeline job for prodci_saas_infra_prov

--- prodci-saas/variables.tf
+++ prodci-saas/variables.tf
@@ -7,15 +7,14 @@ variable "secretKey" {
   description = "AWS secert access key"
 }
 
-# TODO: Update the variables once, pipeline job is added
 variable "region" {
   description = "AWS region"
-  #default = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "avl_zone" {
   description = "availability zone used for the beta"
-  #default = "us-east-1b"
+  default = "us-east-1b"
 }
 
 # all variables related to VPC
@@ -27,17 +26,17 @@ variable "install_version" {
 # START all variables related to VPC
 variable "vpc_id" {
   description = "Prod VPC ID"
-  #default = "vpc-a25c7fc6"
+  default = "vpc-a25c7fc6"
 }
 
 variable "public_subnet_id" {
   description = "Prod CI Public Subnet ID"
-  #default = "subnet-f2abd597"
+  default = "subnet-f2abd597"
 }
 
 # END all variables related to VPC
 
 variable "cidr_private_nat_builds" {
   description = "Private block for nat builds"
-  #default = "172.31.96.0/20"
+  default = "172.31.96.0/20"
 }

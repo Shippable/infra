b80a1f6ae043d838afee000a2ef78b306665ee89
Ragesh Krishna <ragesh@rageshkrishna.com>
2018-12-14 15:50:02 +0530

    [rc] Fix lb names local for CloudWatch

--- rc-saas/sn-public-ship.tf
+++ rc-saas/sn-public-ship.tf
@@ -62,12 +62,12 @@ resource "aws_security_group" "sg_public_lb" {
 # # This normalized list is used to set up the CloudWatch
 # # alarms
 # locals {
-#   lb_mktg_name = "${aws_elb.lb_b_mktg.name}"
-#   lb_api_name = "${aws_elb.lb_b_api.name}"
-#   lb_api_con_name = "${aws_elb.lb_b_api_con.name}"
-#   lb_api_int_name = "${aws_elb.lb_b_api_int.name}"
-#   lb_www_name = "${aws_elb.lb_b_www.name}"
+#   api_lb_names = ["${aws_elb.lb_b_api.name}",
+#     "${aws_elb.lb_b_api_con.name}",
+#     "${aws_elb.lb_b_api_int.name}"
+#   ]
 # }
+
 # # MKTG Load balancer
 # resource "aws_elb" "lb_b_mktg" {
 #   name = "lb-b-mktg-${var.install_version}"
@@ -231,7 +231,10 @@ resource "aws_security_group" "sg_public_lb" {
 # This normalized list is used to set up the CloudWatch
 # alarms
 locals {
-  api_lb_names = ["${aws_elb.lb_g_api.name}", "${aws_elb.lb_g_api_con.name}", "${aws_elb.lb_g_api_int.name}"]
+  api_lb_names = ["${aws_elb.lb_g_api.name}",
+    "${aws_elb.lb_g_api_con.name}",
+    "${aws_elb.lb_g_api_int.name}"
+  ]
 }
 
 # MKTG Load balancer

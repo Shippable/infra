3fe03b5a2784d98751a0c300d20835af4b63a65f
Devashish <devashish.86@gmail.com>
2019-01-28 14:23:40 -0800

    adding test instance

--- rc-saas/sn-public-admiral-setup.tf
+++ rc-saas/sn-public-admiral-setup.tf
@@ -183,7 +183,7 @@ resource "aws_security_group" "sg_public_admiral_setup" {
   vpc_security_group_ids = [
     "${aws_security_group.sg_public_admiral_setup.id}"]
 
-  count = 1
+  count = 2
   root_block_device {
     volume_type = "gp2"
     volume_size = 50

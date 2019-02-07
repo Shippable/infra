1683633fd8a2879096f1c5aae9a5ef7d8e7bb63a
Ranjansingh41 <ranjan@shippable.com>
2019-02-07 20:53:36 +0530

    Removing test instance

--- rc-saas/sn-private-ship-builds.tf
+++ rc-saas/sn-private-ship-builds.tf
@@ -296,27 +296,27 @@ output "drydock_builder" {
 ##   value = "${formatlist("instance %v has private ip %v", aws_instance.test_ric03uec_centos7.*.id, aws_instance.test_ric03uec_centos7.*.private_ip)}"
 ## }
 
- resource "aws_instance" "test_instance_ranjan_u1604" {
-  ami = "${var.ami_us_east_1_ubuntu1604}"
-  availability_zone = "${var.avl-zone}"
-  instance_type = "${var.in_type_core}"
-  key_name = "${var.aws_key_name}"
-  subnet_id = "${aws_subnet.sn_public.id}"
+## resource "aws_instance" "test_instance_ranjan_u1604" {
+##  ami = "${var.ami_us_east_1_ubuntu1604}"
+##  availability_zone = "${var.avl-zone}"
+##  instance_type = "${var.in_type_core}"
+##  key_name = "${var.aws_key_name}"
+##  subnet_id = "${aws_subnet.sn_public.id}"
 
-  vpc_security_group_ids = [
-    "${aws_security_group.sg_private_ship_builds.id}"]
+##  vpc_security_group_ids = [
+##    "${aws_security_group.sg_private_ship_builds.id}"]
 
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 50
-    delete_on_termination = true
-  }
+##  root_block_device {
+##    volume_type = "gp2"
+##    volume_size = 50
+##    delete_on_termination = true
+##  }
 
-  tags = {
-    Name = "test_instance_ranjan_u1604_${var.install_version}"
-  }
-}
+##  tags = {
+##    Name = "test_instance_ranjan_u1604_${var.install_version}"
+##  }
+##}
 
-output "test_instance_ranjan_u1604" {
-  value = "${aws_instance.test_instance_ranjan_u1604.private_ip}"
-}
+##output "test_instance_ranjan_u1604" {
+##  value = "${aws_instance.test_instance_ranjan_u1604.private_ip}"
+##}

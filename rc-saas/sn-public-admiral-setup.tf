2cdb813890f76c7ec7f4e42fba0b34e11757693c
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-05-17 13:59:52 +0530

    Update sn-public-admiral-setup.tf

--- rc-saas/sn-public-admiral-setup.tf
+++ rc-saas/sn-public-admiral-setup.tf
@@ -400,3 +400,31 @@ resource "aws_instance" "admiral_vijayreddy1991_win19" {
 output "admiral_vijayreddy1991_win19" {
   value = "${formatlist("instance %v has private ip %v", aws_instance.admiral_vijayreddy1991_win19.*.id, aws_instance.admiral_vijayreddy1991_win19.*.private_ip)}"
 }
+
+## bindu: x86/64 Ubuntu 16.04 Instances
+resource "aws_instance" "admiral_bindu_u1604" {
+  ami = "${var.ami_us_east_1_ubuntu1604}"
+  availability_zone = "${var.avl-zone}"
+  instance_type = "${var.in_type_ms_x}"
+  key_name = "${var.aws_key_name}"
+  subnet_id = "${aws_subnet.sn_admiral_setup.id}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.sg_public_admiral_setup.id}"]
+
+  count = 2
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 50
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "admiral_bindu_u1604_${count.index}_${var.install_version}"
+  }
+}
+
+
+output "admiral_bindu_u1604" {
+  value = "${formatlist("instance %v has private ip %v", aws_instance.admiral_bindu_u1604.*.id, aws_instance.admiral_bindu_u1604.*.private_ip)}"
+}

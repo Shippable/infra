099ebaf0026dab33480e74dedd16fe3d3dfab7e0
Vishnu Bharathi <vishnubharathi04@gmail.com>
2019-05-24 10:42:29 +0530

    adds Windows Server 2019 for building image

--- rc-saas/sn-public-admiral-setup.tf
+++ rc-saas/sn-public-admiral-setup.tf
@@ -340,6 +340,30 @@ output "admiral_ric03uec_u1604" {
 ##   value = "${formatlist("instance %v has private ip %v", aws_instance.admiral_ric03uec_win16.*.id, aws_instance.admiral_ric03uec_win16.*.private_ip)}"
 ## }
 
+## vishnu:  x86/64 WindowsServer 2019
+resource "aws_instance" "admiral_vishnu_win19" {
+  ami = "${var.ami_us_east_1_win19}"
+  availability_zone = "${var.avl-zone}"
+  instance_type = "${var.in_type_bld}"
+  key_name = "${var.aws_key_name}"
+  subnet_id = "${aws_subnet.sn_admiral_setup.id}"
+  count = 1
+  vpc_security_group_ids = [
+    "${aws_security_group.sg_public_admiral_setup.id}"
+  ]
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 100
+    delete_on_termination = true
+  }
+  tags = {
+    Name = "admiral_vishnu_win19_${count.index}_${var.install_version}"
+  }
+}
+output "admiral_vishnu_win19" {
+  value = "${formatlist("instance %v has private ip %v", aws_instance.admiral_vishnu_win19.*.id, aws_instance.admiral_vishnu_win19.*.private_ip)}"
+}
+
 ## trriplejay: x86/64 Ubuntu 16.04 Instances
 # resource "aws_instance" "admiral_trriplejay_u1604" {
 #   ami = "${var.ami_us_east_1_ubuntu1604}"
@@ -417,4 +441,3 @@ output "admiral_ric03uec_u1604" {
 #output "admiral_varsha_u1804" {
 #  value = "${formatlist("instance %v has private ip %v", aws_instance.admiral_varsha_u1804.*.id, aws_instance.admiral_varsha_u1804.*.private_ip)}"
 #}
-
